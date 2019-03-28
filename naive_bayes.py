727f9e3152a682f6066cfcb65646a743cc29c4bb
Jefferson Santos <jeffersonsantos@localhost.localdomain>
2019-03-27 22:42:54 -0300

    Correcao contagem erros

--- naive_bayes.py
+++ naive_bayes.py
@@ -1,10 +1,11 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import math
+import random as rd
 
 def embaralhar(data_set):
-    data_set = data_set.sample(frac=1).reset_index(drop=True)
-    return data_set
+    return data_set.sample(frac=1, random_state=rd.randint(0,100)).reset_index()
 
 def selecionar_dados_treino(data_set, qtd_treino):
      if qtd_treino > 1:
@@ -27,19 +28,20 @@ def selecionar_json_melhor_classe(evento, data_set, nome_classe):
     for classe in diferentes_classes:
         probabilidade_por_classe.append({
             "classe": classe,
-            "valor": calcular_probabildiade(data_set, classe, nome_classe, evento)
+            "probabilidade": calcular_probabildiade(data_set, classe, nome_classe, evento)
         })
-    # pega o json que tem o maior valor
-    return max(probabilidade_por_classe, key = lambda x: x['valor'])
+    # pega o json que tem a maior probabilidade de todas as classes
+    return max(probabilidade_por_classe, key = lambda x: x['probabilidade'])
     
 def calcular_probabildiade(data_set, valor_classe, nome_classe, evento):
-    data_set_filtrada = data_set.loc[data_set[nome_classe] == valor_classe]    
-    qtd_classe = int(data_set_filtrada.groupby([nome_classe]).size())    
-    frequencia_classe = qtd_classe / len(data_set)
-    fqs = []
+    data_set_filtrada = data_set.loc[data_set[nome_classe] == valor_classe]            
+    qtd_classe = len(data_set_filtrada)
+    fqs = []    
     for index,atrib in enumerate(evento):
         qtd_por_atributo = len(data_set_filtrada.loc[data_set_filtrada[data_set_filtrada.columns[index]].values == atrib])
         fqs.append(qtd_por_atributo/qtd_classe)
+    # por útlimo, a classe
+    fqs.append(qtd_classe / len(data_set))        
     return np.prod(fqs)
 
 
@@ -51,24 +53,34 @@ percent_teste_inicial = 0.1
 percent_treino = percent_treino_inicial
 percent_teste = percent_teste_inicial
 
+erros_totais = []
 # só irá parar quando a quantidade de testes for igual a quantidade de treino inicial
 while percent_teste != percent_treino_inicial:
-    data_set_treino = selecionar_dados_treino(data_set, percent_treino)
-    data_set_teste = selecionar_dados_teste(data_set, percent_treino)
+
+    dados_treino = { "tam_treino": percent_treino * len(data_set), "tam_teste": percent_teste * len(data_set) }
+
+    erros_por_rodada = []
+
+    for i in range(0, 10):                
+        data_set_treino = data_set.sample(frac=percent_treino, random_state=i*rd.randint(0,100)).reset_index(drop=True)
+        data_set_teste = data_set.drop(data_set_treino.index)
+        # pega linha por linha do teste para selecionar a melhor classe e verifica se acertou ou n
+        num_erros = 0
+        for index, linha in data_set_teste.iterrows():
+            classe_teste = linha.Target
+            #remove o target da linha que é a classe e não deve ser considerado
+            del linha["Target"]
+            classe_treino = selecionar_json_melhor_classe(linha, data_set_treino, "Target")["classe"]       
+            if classe_teste != classe_treino:
+                num_erros += 1
+        erros_por_rodada.append(num_erros)
+
+    dados_treino["erro_media"] = np.mean(erros_por_rodada)
+    dados_treino["erro_max"] = np.max(erros_por_rodada)
+    dados_treino["erro_min"] = np.min(erros_por_rodada)
     
-    # pega linha por linha do teste para selecionar a melhor classe e verifica se acertou ou n
-    num_erros = 0
-    for index, linha in data_set_teste.iterrows():
-        classe_teste = linha.Target
-        #remove o target da linha que é a classe e não deve ser considerado
-        del linha["Target"]
-        classe_treino = selecionar_json_melhor_classe(linha, data_set_treino, "Target")["classe"]       
-        if classe_teste != classe_treino:
-            num_erros += 1
-        
+    erros_totais.append(dados_treino)
+
     percent_teste += INCREMETO_TREINO_TESTE 
-    percent_treino += INCREMETO_TREINO_TESTE
-
-
-    
+    percent_treino -= INCREMETO_TREINO_TESTE   
     
