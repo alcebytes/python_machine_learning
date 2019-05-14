c9d19c6b2ff4310f34e22a41b6f6afbb8486a238
Jefferson Santos <jeffersonsantos@localhost.localdomain>
2019-05-13 22:29:36 -0300

    correcao

--- k-means.py
+++ k-means.py
@@ -5,8 +5,27 @@ import math
 import random as rd
 import time
 
+# Cada atributo desse data tem 12 atributos (caracteristicas)
+data_set = pd.read_csv('agrup_centroides_Q1.csv')
+
+
+
+# Não há presença de rótulos, ou seja, classes ou target. Para isso, nossa classe será desconhecida.
+# como não temos, a classe será o indice da "linha"
+
+# começa com 3 centroides, as 3 primeiras linhas.
+centroides = data_set.loc[0:2]
+
+dimensao = list(centroides)
+dimensao.pop(0)
+DIMENSAO = len(dimensao)
+
 def obter_rodada(rodadas, tipo, valor):
-    return [ r for r in rodadas if r[tipo] == valor ]
+    rodadas_rodada = []
+    for r in rodadas:
+        if r[tipo] == valor:
+            rodadas_rodada.append(r)    
+    return rodadas_rodada
 
 def remove_primeira_coluna(linha):
     linha = list(linha)
@@ -27,30 +46,36 @@ def selecionar_centroide_mais_proximo(linha, centroides):
     menor_distancia = distancias_do_centroide.index(min(distancias_do_centroide))
     return centroides.loc[menor_distancia]
 
+def atualizar_centroides(rodadas, diferentes_clusters):
+    novos_centroides = []
+    for i in diferentes_clusters:
+        rodadas_filtrando_cluster = obter_rodada(rodadas, "cluster", float(i))
+        valores = []
+        [valores.append(c["valor"]) for c in rodadas_filtrando_cluster]
+        centroide_novo = []     
+        for coluna in range(0, len(valores)):            
+            for linha in range(0, len(valores)):
+                centroide_novo.append(sum(valores[linha,coluna])/len(valores))            
+        novos_centroides.append(centroide_novo) 
+    return novos_centroides        
 
-# Cada atributo desse data tem 12 atributos (caracteristicas)
-data_set = pd.read_csv('agrup_centroides_Q1.csv')
 
 # rodadas onde é guardado o centroide, em qual rodada está e a classe 
 rodadas = []
 
-# Não há presença de rótulos, ou seja, classes ou target. Para isso, nossa classe será desconhecida.
-# como não temos, a classe será o indice da "linha"
-# começa com 3 centroides, as 3 primeiras linhas.
-centroides = data_set.loc[0:2]
-
 for i,row in data_set.iterrows():        
     centroide = selecionar_centroide_mais_proximo(remove_primeira_coluna(row), centroides)
     valores_centroide = list(centroide)
-    classe = valores_centroide.pop(0)
+    cluster = valores_centroide.pop(0)
     rodadas.append({
-        "centroide" : valores_centroide,
+        "valor" : row,
         "rodada": 1, 
-        "classe": classe
+        "cluster": cluster
     })
 
+centroides = atualizar_centroides(rodadas, [1,2,3])
    
-print(rodadas)
+print(centroides)
  
 
 
