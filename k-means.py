1509a289956592f3113b9f907dc57c9288af5fe3
Jefferson Santos <jeffersonsantos@localhost.localdomain>
2019-05-14 20:07:38 -0300

    Correções

--- k-means.py
+++ k-means.py
@@ -8,17 +8,9 @@ import time
 # Cada atributo desse data tem 12 atributos (caracteristicas)
 data_set = pd.read_csv('agrup_centroides_Q1.csv')
 
-
-
 # Não há presença de rótulos, ou seja, classes ou target. Para isso, nossa classe será desconhecida.
 # como não temos, a classe será o indice da "linha"
-
-# começa com 3 centroides, as 3 primeiras linhas.
-centroides = data_set.loc[0:2]
-
-dimensao = list(centroides)
-dimensao.pop(0)
-DIMENSAO = len(dimensao)
+DIMENSAO = 4
 
 def obter_rodada(rodadas, tipo, valor):
     rodadas_rodada = []
@@ -41,7 +33,7 @@ def distancia_euclidiana(l1, l2):
 def selecionar_centroide_mais_proximo(linha, centroides):
     distancias_do_centroide = []
     for i,centroide in centroides.iterrows():
-        dist = distancia_euclidiana(remove_primeira_coluna(linha), remove_primeira_coluna(centroide))
+        dist = distancia_euclidiana(linha, remove_primeira_coluna(centroide))
         distancias_do_centroide.append(dist)
     menor_distancia = distancias_do_centroide.index(min(distancias_do_centroide))
     return centroides.loc[menor_distancia]
@@ -51,35 +43,37 @@ def atualizar_centroides(rodadas, diferentes_clusters):
     for i in diferentes_clusters:
         rodadas_filtrando_cluster = obter_rodada(rodadas, "cluster", float(i))
         valores = []
-        [valores.append(c["valor"]) for c in rodadas_filtrando_cluster]
+        [valores.append(c["coordenada"]) for c in rodadas_filtrando_cluster]
         centroide_novo = []     
-        for coluna in range(0, len(valores)):            
+        sum_valores = 0
+        for coluna in range(0, DIMENSAO):            
             for linha in range(0, len(valores)):
-                centroide_novo.append(sum(valores[linha,coluna])/len(valores))            
+                sum_valores += valores[linha][coluna]
+            centroide_novo.append(sum_valores/len(valores))            
+            sum_valores = 0
         novos_centroides.append(centroide_novo) 
     return novos_centroides        
+
+#def valida_houve_modificacao_rodadas(rodadas):
+
+    
 
 
 # rodadas onde é guardado o centroide, em qual rodada está e a classe 
 rodadas = []
 
-for i,row in data_set.iterrows():        
-    centroide = selecionar_centroide_mais_proximo(remove_primeira_coluna(row), centroides)
-    valores_centroide = list(centroide)
-    cluster = valores_centroide.pop(0)
-    rodadas.append({
-        "valor" : row,
-        "rodada": 1, 
-        "cluster": cluster
-    })
+# começa com 3 centroides, as 3 primeiras linhas.
+centroides = data_set.loc[0:2]
 
-centroides = atualizar_centroides(rodadas, [1,2,3])
-   
-print(centroides)
- 
-
-
-
-
-
-
+for i in range(0,100):
+    for i,row in data_set.iterrows():    
+        linha_sem_cluster = remove_primeira_coluna(row)    
+        centroide = selecionar_centroide_mais_proximo(linha_sem_cluster, centroides)
+        valores_centroide = list(centroide)
+        cluster = valores_centroide.pop(0)
+        rodadas.append({
+            "coordenada" : linha_sem_cluster,
+            "rodada": 1, 
+            "cluster": cluster
+        })
+    centroides = atualizar_centroides(rodadas, [1,2,3])
